8272f015683a02d75960109fdf03ccf631ba021a
edeeb4 <liza61198@gmail.com>
2026-01-27 13:05:15 -0500

    all UI linked- no API

--- js/app.js
+++ js/app.js
@@ -1,3 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Front end loaded");
+  console.log("app.js is loaded");
+
+    //landing
+    if (document.getElementById("goLogin")) wireLanding();
+
+    //login functions
+    if (document.getElementById("loginBtn")) wireLogin();
+
+    //creating new account
+    if (document.getElementById("regBtn")) wireSignup();
+
+    //contacts
+    if (document.getElementById("searchBtn")) wireContacts();
+
 });
+
+//connect pages
+function wireLanding(){
+    document.getElementById("goLogin").onclick = () => location.href = "login.html";
+    document.getElementById("goSignup").onclick = () => location.href = "signup.html";
+}
+
+//process of logging in
+function wireLogin(){
+    const back = document.getElementById("backHome")
+    if (back) back.onclick = () =>location.href = "index.html";
+
+    document.getElementById("loginBtn").onclick = () => {
+        console.log("Login Clicked")
+        //Placeholder since I dont have API yet
+        document.getElementById("status").textContent = "Login clicked (no API yet)";
+
+        //Login was succesful - pretend login for right now
+        localStorage.setItem("loggedIn", "true");
+        location.href = "contacts.html";
+        };
+}
+
+function wireSignup(){
+    const back = document.getElementById("backHome");
+    if (back) back.onclick = () => location.href = "index.html";
+
+    document.getElementById("regBtn").onclick = () => {
+    console.log("Signup Clicked")
+    //no API yet
+    document.getElementById("status").textContent = "Signup Completed (no API yet)";
+    };
+}
+
+function wireContacts(){
+//basic login success- again need to wait for API
+if(localStorage.getItem("loggedIn")!== "true"){
+    location.href = "index.html";
+    return;
+}
+
+document.getElementById("logoutBtn").onclick = () => {
+    localStorage.removeItem("loggedIn");
+    location.href = "index.html";
+};
+}
