45f600832318091802361a9c399120c9b95ce825
edeeb4 <liza61198@gmail.com>
2026-02-13 02:23:03 -0500

    login and signup functions coded in js. api linked

--- js/app.js
+++ js/app.js
@@ -1,69 +1,115 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("app.js is loaded");
+// var urlBase = ''website whenever we have
+const urlBase = "http://localhost:8000";
+const extension = 'php';
 
-    //landing
-    if (document.getElementById("goLogin")) wireLanding();
-
-    //login functions
-    if (document.getElementById("loginBtn")) wireLogin();
-
-    //creating new account
-    if (document.getElementById("regBtn")) wireSignup();
-
-    //contacts
-    if (document.getElementById("searchBtn")) wireContacts();
-
-});
-
-//main page
-function wireLanding(){
-
-}
-
-//process of logging in
-function wireLogin(){
-
-    document.getElementById("loginBtn").onclick = () => {
-        console.log("Login Clicked")
-        //Placeholder since I dont have API yet
-        document.getElementById("status").textContent = "Login clicked (no API yet)";
-
-        //Login was succesful - pretend login for right now
-        localStorage.setItem("loggedIn", "true");
-
-        //pretend Backend 
-        localStorage.setItem("firstName", "juan");
-        localStorage.setItem("lastName", "Loves");
+let userId = 0;
+let firstName, lastName = "";
 
 
+//create new account function
+function signUp(){
+    let userId = 0;
+    let firstName = document.getElementById("regFirst").value;
+    let lastName = document.getElementById("regLast").value;
+    let username = document.getElementById("regLogin").value;
+    let password = document.getElementById("regPassword").value;
+    let hash = md5(password);
+
+    document.getElementById("status").innerHTML = "";
+
+    let newInfo = {
+        firstName: firstName,
+        lastName: lastName,
+        login: username,
+        password: hash,
     };
+    let jsonPayload = JSON.stringify(newInfo);
+    let regURL = urlBase + '/Register.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", regURL, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+
+    try{
+        xhr.onreadystatechange = function (){
+            if (this.readyState === 4 && this.status === 200) {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if (userId < 1) {
+                    console.log(userId);
+                    document.getElementById("status").innerHTML = "A problem occured while trying to create your new account";
+                    return;
+                }
+
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+
+                window.location.href = "contacts.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err) {
+        document.getElementById("status").innerHTML = err.message;
+    }
 }
 
-function wireSignup(){
-    const back = document.getElementById("backHome");
-    if (back) back.onclick = () => location.href = "index.html";
 
-    document.getElementById("regBtn").onclick = () => {
-        console.log("Signup Clicked")
-        //no API yet
-        document.getElementById("status").textContent = "Signup Completed (no API yet)";
-    };
-}
 
-function wireContacts(){
-//basic login success- again need to wait for API
-    if(localStorage.getItem("loggedIn")!== "true"){
-        location.href = "index.html";
-        return;
+//login function
+function goLogin(){
+    userId = 0;
+    firstName = "";
+    lastName = "";
+
+    let username = document.getElementById("loginuser").value;
+    let password = document.getElementById("loginpassword").value;
+    let hash = md5( password );
+
+    document.getElementById("status").innerHTML = "";
+
+    let tmp = {
+        login: username,
+        password: hash };
+    //	var tmp = {login:login,password:hash};
+    let jsonPayload = JSON.stringify(tmp);
+
+    let url = urlBase + '/Login.' + extension;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try {
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                let jsonObject = JSON.parse(xhr.responseText);
+                userId = jsonObject.id;
+
+                if (userId < 1) {
+                    console.log(userId);
+                    document.getElementById("status").innerHTML = "User/Password combination incorrect";
+                    return;
+                }
+
+                firstName = jsonObject.firstName;
+                lastName = jsonObject.lastName;
+
+
+
+                window.location.href = "contacts.html";
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch (err) {
+        document.getElementById("status").innerHTML = err.message;
     }
 
-    setWelcomeText();
-
-    document.getElementById("logoutBtn").onclick = () => {
-        localStorage.removeItem("loggedIn");
-        location.href = "index.html";
-    };
 }
+
+
 
 //show password checkbox
 function showPassword()
