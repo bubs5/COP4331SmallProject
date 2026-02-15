d7ab78be308fb96334b238ef1d1945d062c0c820
Isabella Miller <isabella@cedillas.com>
2026-02-14 19:12:50 -0500

    Update SearchContacts.php
    database name changed to CONTACTSPROJ

--- SearchContacts.php
+++ SearchContacts.php
@@ -5,14 +5,14 @@
 	$searchResults = "";
 	$searchCount = 0;
 
-	$conn = new mysqli("localhost", "root", getPassword(), "COP4331"); 
+	$conn = new mysqli("localhost", "root", getPassword(), "CONTACTSPROJ"); 
 	if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-		$stmt = $conn->prepare("select Name from Contacts where Name like ?");
+		$stmt = $conn->prepare("select * from Contacts where FirstName like ? or LastName like ?");
 		$user = "%" . $inData["search"] . "%";
 		$stmt->bind_param("ss", $user);
 		$stmt->execute();
@@ -66,4 +66,5 @@
 	}
 	
 ?>
+
 
