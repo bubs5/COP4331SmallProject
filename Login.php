d9a2b09b170d16622f0ffb55bbd40fc62bfb02bb
Isabella Miller <isabella@cedillas.com>
2026-02-14 19:05:09 -0500

    Update Login.php
    Changed database name to CONTACTSPROJ

--- Login.php
+++ Login.php
@@ -7,7 +7,7 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "root", getPassword(), "COP4331"); 	
+	$conn = new mysqli("localhost", "root", getPassword(), "CONTACTSPROJ"); 	
 	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
@@ -64,6 +64,7 @@
 	}
 	
 ?>
+
 
 
 
