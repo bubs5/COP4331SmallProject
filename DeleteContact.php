f32a60b8aabe212e4b39be30cd4594e8a9ffa4e7
Isabella Miller <isabella@cedillas.com>
2026-02-14 19:02:04 -0500

    Update DeleteContact.php

--- DeleteContact.php
+++ DeleteContact.php
@@ -5,7 +5,7 @@
 	$contactID = $inData["contactId"];
 	$userID    = $inData["userId"];
 
-	$conn = new mysqli("localhost", "root", getPassword(), "COP4331");
+	$conn = new mysqli("localhost", "root", getPassword(), "CONTACTSPROJ");
 	if ($conn->connect_error)
 	{
 		returnWithError($conn->connect_error);
